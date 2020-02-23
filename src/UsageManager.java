3460bc17bc1547895e080a7bb4470141a6f1595c
Kurt Daill <Daill.Kurt@gmail.com>
2020-02-23 12:52:26 -0600

    Added Tuple Class for easier Data Storage

--- src/UsageManager.java
+++ src/UsageManager.java
@@ -4,8 +4,5 @@ public abstract class UsageManager extends FacilityTracker{
 	boolean assignFacilityToUse(String facName, TimeStamp usage) {
 		return true;
 	}
-	boolean vacateFacility(String facName, TimeStamp vacateTime) {
-		return true;
-	}
-	
+	boolean vacateFacility(String facName, TimeStamp vacateTime) { return true; }
 }
