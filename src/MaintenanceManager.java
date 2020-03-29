408d2af7cf850d254bb023845e0534138f0e966b
KurtDaill <Daill.Kurt@gmail.com>
2020-03-28 20:32:14 -0500

    began refactoring classes

--- src/MaintenanceManager.java
+++ src/MaintenanceManager.java
@@ -1,14 +1,17 @@
-import java.util.*; 
+import src.MaintTimeStamp;
+import src.ScheduleManager;
+import sun.applet.Main;
 
+import java.util.*;
+import java.util.Map;
 public class MaintenanceManager {
-	private FacilityManager parentManager;
-	//List<String> pendingRequestsName = new LinkedList<String>();
-	//List<TimeStamp> pendingRequestsTime = new LinkedList<TimeStamp>();
 	private List<Tuple<String, TimeStamp>> pendingRequests = new LinkedList<Tuple<String, TimeStamp>>();
-	
-	void scheduleMaintenance(String facName, MaintTimeStamp maintenance) {
+	private Map<String, List<MaintTimeStamp>> maintDirectory = new HashMap<String, List<MaintTimeStamp>>();
+	private Map<String, List<MaintTimeStamp>> maintHistoryDirectory = new HashMap<String, List<MaintTimeStamp>>();
+	private ScheduleManager schedule;
 
-		parentManager.lookUp(facName).addMaint(maintenance);
+	void scheduleMaintenance(String facName, MaintTimeStamp maintenance) {
+		maintDirectory.get(facName).add(maintenance);
 	}
 
 	void makeFacilitymaintenanceRequest(TimeStamp maintenance, String facName) {
@@ -17,5 +20,30 @@ public class MaintenanceManager {
 
 	List<Tuple<String, TimeStamp>> listMaintRequests(){
 		return pendingRequests;
+	}
+
+	List<MaintTimeStamp>getMaintHistory(String facName){
+		return maintHistoryDirectory.get(facName);
+	}
+
+	void addMaintainance(MaintTimeStamp maint, String facName){
+		maintDirectory.get(facName).add(maint);
+	}
+
+	List<MaintTimeStamp> listMaintainance(String facName){
+		return maintDirectory.get(facName);
+	}
+
+	float calcMaintCostForFacility(String facName){
+		float cost = 0;
+		List<MaintTimeStamp> hist = maintHistoryDirectory.get(facName);
+		for(MaintTimeStamp maint : hist){
+			cost += maint.getCostUSD();
+		}
+		return cost;
+	}
+
+	void update(){
+
 	}
 }
